19ddfb0bc4779ee71969ed28673c3b198f78a10f
Jenkins <james@somehero.com>
2013-04-07 17:37:18 -0400

    removed some controllers

--- app/controllers/phonecampaigns_controller.rb
+++
@@ -1,9 +0,0 @@
-class PhonecampaignsController < InheritedResources::Base
-	layout 'actions'
-
-	def view
-		@phonecampaign = PhoneCampaign.find_by_rewrite_url_key(params[:action_title])
-
-		render :show
-	end
-end
