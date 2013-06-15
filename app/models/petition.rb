0de1c9fb60037bdf8f86dd2ce3d630f9d2f8e6f0
Jenkins <james@somehero.com>
2013-06-15 01:37:25 -0400

    next pass at new design

--- app/models/petition.rb
+++ app/models/petition.rb
@@ -4,7 +4,6 @@ class Petition < ActiveRecord::Base
   belongs_to :target
   belongs_to :user
   has_many :signatures, :order => 'created_at DESC', :limit => 10
-  attr_accessible :rewrite_url_key, :short_url, :summary, :target_count, :title
   validates :title, :presence => true
   validates :summary, :presence => true
   validates :target_count, :numericality => { :only_integer => true }
