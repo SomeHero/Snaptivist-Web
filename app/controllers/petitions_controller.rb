cbf18990f096efef8a2a65b73aad97c3a64761ac
Jenkins <james@somehero.com>
2013-07-06 12:54:32 -0400

    fixed issue when petition is not found

--- app/controllers/petitions_controller.rb
+++ app/controllers/petitions_controller.rb
@@ -6,6 +6,8 @@ class PetitionsController < InheritedResources::Base
 		@petition = Petition.includes(:target).includes(:signatures)
 		.find_by_rewrite_url_key(request.subdomain.gsub(".localhost", ""))
 
+		raise "Unable to find petition" unless @petition
+
 		render :show
 	end
 
