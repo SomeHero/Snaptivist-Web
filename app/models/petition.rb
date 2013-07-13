a18a9c93b39b17782aa1f07dd857236399fd0d80
Jenkins <james@somehero.com>
2013-07-13 00:54:49 -0400

    some active admin stuff

--- app/models/petition.rb
+++ app/models/petition.rb
@@ -39,46 +39,47 @@ class Petition < ActiveRecord::Base
 
     end
 
-  # get URL to image of a specific size
-  def p_image_url(label = nil, use_pre_publish = false)
-    if use_pre_publish && !p_pre_publish_image_file_name.blank?
-      if label.nil?
-        p_pre_publish_image.url
+  public
+    # get URL to image of a specific size
+    def p_image_url(label = nil, use_pre_publish = false)
+      if use_pre_publish && !p_pre_publish_image_file_name.blank?
+        if label.nil?
+          p_pre_publish_image.url
+        else
+          p_pre_publish_image.url(label)
+        end
+      elsif p_image_file_name.blank?
+        Product.default_image(label)
+      elsif label.nil?
+        p_image.url
       else
-        p_pre_publish_image.url(label)
+        p_image.url(label)
       end
-    elsif p_image_file_name.blank?
-      Product.default_image(label)
-    elsif label.nil?
-      p_image.url
-    else
-      p_image.url(label)
-    end
-  end
-
-   # generate the petition
-  def to_api
-
-    results = {
-      'petition_id' => id,
-      'title' => title,
-      'summary' => summary,
-      'signature_count' => signatures_count,
-      'target_count' => target_count,
-      'short_url' => short_url,
-      'rewrite_url_key' => rewrite_url_key,
-      'target' => target.to_api,
-      'creator' => user.to_api,
-      'created_at' => created_at,
-      'updated_at' => updated_at,
-    }
-
-    Petition::IMAGE_SIZES.each do |label, size|
-      results["image_#{label}"] = header_image(label)
     end
 
-    return results;
+     # generate the petition
+    def to_api
 
-  end
+      results = {
+        'petition_id' => id,
+        'title' => title,
+        'summary' => summary,
+        'signature_count' => signatures_count,
+        'target_count' => target_count,
+        'short_url' => short_url,
+        'rewrite_url_key' => rewrite_url_key,
+        'target' => target.to_api,
+        'creator' => user.to_api,
+        'created_at' => created_at,
+        'updated_at' => updated_at,
+      }
+
+      Petition::IMAGE_SIZES.each do |label, size|
+        results["image_#{label}"] = header_image(label)
+      end
+
+      return results;
+
+    end
   
 end
