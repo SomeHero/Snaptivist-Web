23a429f809c3e1c6ba382e0084b3d4500383dde2
Jenkins <james@somehero.com>
2013-07-16 09:01:14 -0400

    fix issue with not sending email with facebook signature

--- app/controllers/api/petitions_controller.rb
+++ app/controllers/api/petitions_controller.rb
@@ -212,12 +212,12 @@ class Api::PetitionsController < ApplicationController
 
       #send petition action email
       UserNotification::UserNotificationRouter.instance.notify_user(UserNotification::Notification::USER_WELCOME, :user => user, :merge_fields => {
-          "merge_petitiontitle" => @petition.title,
-          "merge_firstname" => @user.first_name,
-          "merge_lastname" => @user.last_name,
-          "merge_targetname" => @petition.target.title + " " + @petition.target.last_name,
-          "merge_shorturl" => @petition.short_url,
-          "merge_organizationname" => @petition.user.organization_name
+          "merge_petitiontitle" => petition.title,
+          "merge_firstname" => user.first_name,
+          "merge_lastname" => user.last_name,
+          "merge_targetname" => petition.target.title + " " + petition.target.last_name,
+          "merge_shorturl" => petition.short_url,
+          "merge_organizationname" => petition.user.organization_name
       })
 
     end
