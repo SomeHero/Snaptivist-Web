1aa1d7df25a7cc758d3bbb647bf9626ffe890e72
jrhodes621 <james@somehero.com>
2014-01-09 22:20:23 -0500

    a couple more crm issues

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -22,7 +22,8 @@ class OauthController < ApplicationController
 	jsonObject = JSON.parse response.body
 	access_token = jsonObject['access_token']
 
-	client.nation_builder_crm_authentication = NationBuilderCrmAuthentication.create!(
+	NationBuilderCrmAuthentication.create!(
+		client: client,
 		nation_name: session['nation_name'],
 		client_app_id: session['client_app_id'],
 		client_secret: session['client_secret'],
--- app/models/nation_builder_crm_authentication.rb
+++ app/models/nation_builder_crm_authentication.rb
@@ -1,7 +1,7 @@
 class NationBuilderCrmAuthentication < ActiveRecord::Base
 	has_one :client
   
-    attr_accessible :access_token, :client_app_id, :client_id, :client_secret, :nation_name, :redirect_uri
+    attr_accessible :client, :access_token, :client_app_id, :client_id, :client_secret, :nation_name, :redirect_uri
 
 	def to_api
 
