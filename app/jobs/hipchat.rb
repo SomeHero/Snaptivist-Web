b13179f6a8079c0bd580ae3c19c00b275976424f
jrhodes621 <james@somehero.com>
2014-01-14 11:47:12 -0500

    fixed typo

--- app/jobs/hipchat.rb
+++ app/jobs/hipchat.rb
@@ -10,7 +10,7 @@ module Hipchat
     from = "SnapMon"
 
     user = User.find(signature["user_id"])
-    message = user.first_name + " " + user.last_name + " (" + user.email + ") just signed the petition " + petition["title"] ".  The petition has been signed " + petitions["signatures_count"] + " times."
+    message = user.first_name + " " + user.last_name + " (" + user.email + ") just signed the petition " + petition["title"] + ".  The petition has been signed " + petitions["signatures_count"] + " times."
 
     hipchat_api.rooms_message(room_id, from, message, notify = 0, color = 'yellow', message_format = 'html')
     puts "Posted message to HipChat!"
