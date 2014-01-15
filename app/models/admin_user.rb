47facd665957a96dddd17c8fa28511495829d085
jrhodes621 <james@somehero.com>
2014-01-15 09:33:49 -0500

    added username to adminuser class

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -6,6 +6,8 @@ class AdminUser < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+  #validates :username, uniqueness: true, if: -> { self.username.present? }
+
 end
