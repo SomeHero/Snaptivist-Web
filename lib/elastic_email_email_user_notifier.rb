0be06bf5e024186ce57d7e415062cfe8ffea2bc4
jrhodes621 <james@somehero.com>
2014-01-16 13:04:51 -0500

    fixed from name and address

--- lib/elastic_email_email_user_notifier.rb
+++ lib/elastic_email_email_user_notifier.rb
@@ -28,26 +28,35 @@ module UserNotification
         result = ElasticEmailApi.send_email(user.email, subject, template_name, from_address, from_name, merge_fields)
       when UserNotification::Notification::SIGNATURE_CONFIRMATION 
         user = params_hash[:user]
+        
         template_name = "Signature Confirmation"
+        from_address = params_hash[:from_address] || Settings.default_from
+        from_name = params_hash[:from_name]|| Settings.default_from_name
         subject = "Your petition signature"
 
         merge_fields = params_hash[:merge_fields]
-        result = ElasticEmailApi.send_email(user.email, subject, template_name, Settings.default_from, Settings.default_from_name, merge_fields)
+        result = ElasticEmailApi.send_email(user.email, subject, template_name, from_address, from_name, merge_fields)
       when UserNotification::Notification::SIGNATURE_DELIVERY_REMINDER
         user = params_hash[:user]
+        
         template_name = "Delivery Follow Up"
+        from_address = params_hash[:from_address] || Settings.default_from
+        from_name = params_hash[:from_name]|| Settings.default_from_name
         subject = "Your Message Wasn't Delivered"
 
         merge_fields = params_hash[:merge_fields]
-        result = ElasticEmailApi.send_email(user.email, subject, template_name, Settings.default_from, Settings.default_from_name, merge_fields)
+        result = ElasticEmailApi.send_email(user.email, subject, template_name, from_address, from_name, merge_fields)
       when UserNotification::Notification::DONATION_REMINDER
         user = params_hash[:user]
+        
         template_name = params_hash[:template_name] || "Test Donation Reminder Email"
+        from_address = params_hash[:from_address] || Settings.default_from
+        from_name = params_hash[:from_name]|| Settings.default_from_name
         subject = params_hash[:subject] || "Donation Reminder Email"
         time_offset_minutes = params_hash[:time_offset_minutes]
 
         merge_fields = params_hash[:merge_fields]
-        result = ElasticEmailApi.send_email(user.email, subject, template_name, Settings.default_from, Settings.default_from_name, merge_fields,
+        result = ElasticEmailApi.send_email(user.email, subject, template_name, from_address, from_name, merge_fields,
           "", "", "", time_offset_minutes)
       else
           raise "I don't know how to handle notifications of type '#{notification_type}'!"
