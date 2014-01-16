d7b082cc98f43b2b72040e0c20e8f4daf19f7b97
jrhodes621 <james@somehero.com>
2014-01-15 20:40:05 -0500

    fixed typo

--- lib/nation_builder_web_hook.rb
+++ lib/nation_builder_web_hook.rb
@@ -36,7 +36,7 @@ module CrmWebHook
 			user.save
   		end
 
-  		REDIS.set("user-" + user.id.to_s, payload.)
+  		REDIS.set("user-" + user.id.to_s, payload)
   	end
 
   	def create_or_update_donation(payload, client)
