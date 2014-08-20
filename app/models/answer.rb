fd45264468bd864306b3d9ec31464ad14889190f
jrhodes621 <james@somehero.com>
2014-08-20 11:41:04 -0400

    drop answer table

--- app/models/answer.rb
+++
@@ -1,5 +0,0 @@
-class Answer < ActiveRecord::Base
-  #attr_accessible :choice_id, :poll_id
-  belongs_to :choice
-  belongs_to :poll
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140818170754) do
+ActiveRecord::Schema.define(version: 20140820153634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,13 +73,6 @@ ActiveRecord::Schema.define(version: 20140818170754) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "answers", force: true do |t|
-    t.integer  "question_id"
-    t.integer  "choice_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
 
   create_table "authentications", force: true do |t|
     t.integer  "user_id"
